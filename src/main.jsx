d3aa7ab89665da3f3b682c420e628c5abf3a1e63
konstantin <refundsix@mail.ru>
2025-01-27 20:04:31 +0700

    auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,17 @@
-import React, { useState, useEffect } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { MemoryRouter } from "react-router-dom";
-import App from "./App";
-import Loader from "./components/Loader";
 import WebApp from "@twa-dev/sdk";
-
-function RootComponent() {
-  const [isInitialLoading, setIsInitialLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsInitialLoading(false);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (isInitialLoading) {
-    return <Loader />;
-  }
-
-  return <App />;
-}
+import App from "./App";
+import "./index.css";
 
 WebApp.ready();
 WebApp.expand();
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <MemoryRouter>
-    <RootComponent />
-  </MemoryRouter>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  </StrictMode>
 );
