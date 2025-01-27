004e274aa3160267972ead4e4b6bcca189b7c4d2
konstantin <refundsix@mail.ru>
2025-01-27 19:19:27 +0700

    auth

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
     <div className="fixed inset-0 bg-gray-50 flex items-center justify-center">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-black z-10"></div>
+      <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-black z-10"></div>
     </div>
   );
 };
